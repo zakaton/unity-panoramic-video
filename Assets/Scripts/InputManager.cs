8cd93ca051e6af228f5c40a0489250ababcc6f0d
Zack <zack@ukaton.com>
2023-07-29 21:33:09 -0700

    .

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,110 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private VideoManager videoManager = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public OVRHand leftHand = null;
+	private bool isLeftHandPinching = false;
+	public OVRHand rightHand = null;
+	private bool isRightHandPinching = false;
+
+	// https://github.com/jemmec/metaface-utilities/blob/master/Assets/Metaface/Scripts/Blinking/BlinkHelper.cs
+	private OVRFaceExpressions faceExpressions = null;
+
+	private bool hasStartedBlink;
+	private float blinkTime;
+
+	private void Start()
+	{
+		videoManager = GetComponent<VideoManager>();
+		faceExpressions = GetComponent<OVRFaceExpressions>();
+	}
+
+	private void Update()
+	{
+		if (!videoManager.IsVideoReady)
+			return;
+
+		HandTrackingInput();
+		KeyboardInput();
+		FaceExpressionsInput();
+	}
+
+	private void HandTrackingInput()
+	{
+		bool _isRightHandPinching = rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+		if (_isRightHandPinching != isRightHandPinching)
+		{
+			isRightHandPinching = _isRightHandPinching;
+			if (isRightHandPinching)
+			{
+				videoManager.PauseToggle();
+			}
+		}
+	}
+
+	private void FaceExpressionsInput()
+	{
+		if (!faceExpressions) return;
+
+		bool areEyesClosed = AreEyesClosed();
+
+
+		if (!hasStartedBlink)
+		{
+			if (areEyesClosed)
+			{
+				hasStartedBlink = true;
+				blinkTime = 0f;
+			}
+		}
+		else
+		{
+			blinkTime += Time.deltaTime;
+			if (!areEyesClosed)
+			{
+				hasStartedBlink = false;
+				videoManager.PauseToggle();
+			}
+		}
+	}
+
+	private bool AreEyesClosed()
+	{
+		float leftEyeClosedWeight;
+		faceExpressions.TryGetFaceExpressionWeight(OVRFaceExpressions.FaceExpression.EyesClosedL, out leftEyeClosedWeight);
+		float rightEyeClosedWeight;
+		faceExpressions.TryGetFaceExpressionWeight(OVRFaceExpressions.FaceExpression.EyesClosedR, out rightEyeClosedWeight);
+		bool areEyesClosed = leftEyeClosedWeight < 0.5 && rightEyeClosedWeight < 0.5;
+		return areEyesClosed;
+	}
+
+	private void KeyboardInput()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			videoManager.PauseToggle();
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			videoManager.PreviousVideo();
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			videoManager.NextVideo();
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			videoManager.SeekBack();
+		}
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			videoManager.SeekForward();
+		}
+	}
 }
--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,6 +6,9 @@ using UnityEngine.Video;
 public class VideoManager : MonoBehaviour
 {
 	public List<VideoClip> videos = null;
+
+	public VideoEvent onPause = new VideoEvent();
+	public VideoEvent onLoad = new VideoEvent();
 
 	private bool isPaused = false;
 	public bool IsPaused
@@ -18,7 +20,7 @@ public class VideoManager : MonoBehaviour
 		private set
 		{
 			isPaused = value;
-
+			onPause.Invoke(isPaused);
 		}
 	}
 
@@ -32,6 +34,7 @@ public class VideoManager : MonoBehaviour
 		private set
 		{
 			isVideoReady = value;
+			onLoad.Invoke(isVideoReady);
 		}
 	}
 
@@ -41,15 +44,86 @@ public class VideoManager : MonoBehaviour
 	public void Awake()
 	{
 		videoPlayer = GetComponent<VideoPlayer>();
+		videoPlayer.seekCompleted += OnComplete;
+		videoPlayer.prepareCompleted += OnComplete;
+		videoPlayer.loopPointReached += OnLoop;
+	}
+
+	private void Start()
+	{
+		StartPrepare(index);
 	}
 
 	public void PauseToggle()
 	{
 		IsPaused = !videoPlayer.isPaused;
 
+		print(IsPaused);
+
 		if (IsPaused)
 			videoPlayer.Pause();
 		else
 			videoPlayer.Play();
 	}
+
+	private void OnDestroy()
+	{
+		videoPlayer.seekCompleted -= OnComplete;
+		videoPlayer.prepareCompleted -= OnComplete;
+		videoPlayer.loopPointReached -= OnLoop;
+	}
+
+	public void SeekForward()
+	{
+		StartSeek(10.0f);
+	}
+	public void SeekBack()
+	{
+		StartSeek(-10.0f);
+	}
+
+	public void StartSeek(float seekAmount)
+	{
+		IsVideoReady = false;
+		videoPlayer.time += seekAmount;
+	}
+
+	public void NextVideo()
+	{
+		index++;
+
+		if (index == videos.Count)
+			index = 0;
+
+		StartPrepare(index);
+	}
+	public void PreviousVideo()
+	{
+		index--;
+
+		if (index == -1)
+			index = videos.Count - 1;
+
+		StartPrepare(index);
+	}
+
+	private void StartPrepare(int clipIndex)
+	{
+		IsVideoReady = false;
+		videoPlayer.clip = videos[clipIndex];
+		videoPlayer.Prepare();
+	}
+
+	private void OnComplete(VideoPlayer videoPlayer)
+	{
+		IsVideoReady = true;
+		videoPlayer.Play();
+	}
+
+	private void OnLoop(VideoPlayer videoPlayer)
+	{
+		NextVideo();
+	}
+
+	public class VideoEvent : UnityEvent<bool> { }
 }
